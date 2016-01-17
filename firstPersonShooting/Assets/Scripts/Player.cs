86b72502980ffab2787065b55585bcf0307b98a2
whg333 <whg3.3.3@163.com>
2016-01-18 02:42:10 +0800

    add firstPersonShooting sample

---
+++ firstPersonShooting/Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private CharacterController characterController;
+	private Transform cameraTransform;
+	private Vector3 cameraAngles;
+	private float cameraHeight = 1.4f;
+
+	private float gravity = 9.81f;
+	public float moveSpeed =  4.0f;
+	private float jumpSpeed =  30.0f;
+
+	public int hp;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+		cameraTransform = Camera.main.transform;
+
+		Vector3 startPos = transform.position;
+		startPos.y += cameraHeight;
+		cameraTransform.position = startPos;
+
+		cameraTransform.rotation = transform.rotation;
+		cameraAngles = cameraTransform.eulerAngles;
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(IsDeath()){
+			return;
+		}
+		Move();
+	}
+
+	public bool IsDeath(){
+		return hp <= 0;
+	}
+
+	void Move(){
+		MoveCamera();
+		MovePlayer();
+
+		Vector3 pos = transform.position;
+		pos.y += cameraHeight;
+		cameraTransform.position = pos;
+	}
+
+	void MoveCamera(){
+		float y = Input.GetAxis("Mouse X");
+		float x = Input.GetAxis("Mouse Y");
+
+		cameraAngles.x -= x;
+		cameraAngles.y += y;
+		cameraTransform.eulerAngles = cameraAngles;
+
+		Vector3 cameraDirection = cameraAngles;
+		cameraDirection.x = 0;
+		cameraDirection.z = 0;
+		transform.eulerAngles = cameraDirection;
+	}
+
+	void MovePlayer(){
+		float x = 0, y = 0, z = 0;
+
+		if(Input.GetKey(KeyCode.D)){
+			x += moveSpeed * Time.deltaTime;
+		}else if(Input.GetKey(KeyCode.A)){
+			x -= moveSpeed * Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.Space)){
+			y += jumpSpeed * Time.deltaTime;
+		}else if(transform.position.y-cameraHeight > 0){
+			y -= gravity * Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.W)){
+			z += moveSpeed * Time.deltaTime;
+		}else if(Input.GetKey(KeyCode.S)){
+			z -= moveSpeed * Time.deltaTime;
+		}
+
+		characterController.Move(transform.TransformDirection(new Vector3(x, y, z)));
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawIcon(transform.position, "../Spawn.tif");
+	}
+
+}
---
+++ firstPersonShooting/Assets/Scripts/Zombie.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (NavMeshAgent))]
+[RequireComponent (typeof (Animator))]
+public class Zombie : MonoBehaviour {
+
+	//玩家
+	private Player player;
+
+	//寻路代理
+	private NavMeshAgent agent;
+
+	//动画
+	private Animator animator;
+
+	public float moveSpeed = 0.5f;
+
+	public int hp;
+
+	private float rotateSpeed = 120;
+
+	private float actTimer = 2;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.IsDeath()){
+			return;
+		}
+			
+		if (animator.IsInTransition (0)) {
+			return;
+		}
+
+		//基于状态机的判断播放相应的行为动画
+		AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+		if(InState(state, "idle")){
+			animator.SetBool("idle", false);
+
+			actTimer -= Time.deltaTime;
+			if(actTimer > 0){
+				return;
+			}
+
+			if(DistanceToPlayerLessEnough()) {
+				animator.SetBool ("attack", true);
+			}else{
+				animator.SetBool("run", true);
+				//agent.SetDestination(player.transform.position);
+				actTimer = 1;
+			}
+		}else if(InState(state, "run")){
+			animator.SetBool("run", false);
+
+			if(DistanceToPlayerLessEnough ()){
+				animator.SetBool ("attack", true);
+				agent.ResetPath ();
+				return;
+			}else{
+				actTimer -= Time.deltaTime;
+				if(actTimer < 0){
+					agent.SetDestination(player.transform.position);
+					actTimer = 1;
+				}
+
+				//发现其实只需要agent.SetDestination便可以自动寻路了，都不需要调用agent.Move
+				//MoveToPlayer();
+			}
+		}else if(InState(state, "attack")){
+			animator.SetBool("attack", false);
+
+			RotateToPlayer();
+
+			if(state.normalizedTime >= 1f){
+				animator.SetBool("idle", true);
+				actTimer = 2;
+			}
+		}
+	}
+
+	bool InState(AnimatorStateInfo state, string stateStr){
+		//return state.fullPathHash == Animator.StringToHash("Base Layer."+stateStr);
+		//使用如下的IsName方法替代上面的哈希值判断
+		return state.IsName(stateStr);
+	}
+
+	bool DistanceToPlayerLessEnough(){
+		return DistanceToPlayerLessThan(1.5f);
+	}
+
+	bool DistanceToPlayerLessThan(float distance){
+		return Vector3.Distance (transform.position, player.transform.position) < distance;
+	}
+
+	/*
+	void MoveToPlayer(){
+		agent.SetDestination(player.transform.position);
+		agent.Move(transform.TransformDirection(new Vector3(0, 0, moveSpeed*Time.deltaTime)));
+	}
+	*/
+
+	void RotateToPlayer(){
+		Vector3 old = transform.eulerAngles;
+		transform.LookAt(player.transform);
+		float playerY = transform.eulerAngles.y;
+
+		float speed = rotateSpeed * Time.deltaTime;
+		float angle = Mathf.MoveTowardsAngle(old.y, playerY, speed);
+		transform.eulerAngles = new Vector3(0, angle, 0);
+	}
+
+}
